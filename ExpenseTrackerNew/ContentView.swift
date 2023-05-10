c17651246c12e99117b8bdca9866d6f83fd08718
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2023-05-10 14:15:39 -0700

    Initial Commit

---
+++ ExpenseTrackerNew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerNew
+//
+//  Created by Mohosin Islam Palash on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTrackerNew/ExpenseTrackerNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerNewApp.swift
+//  ExpenseTrackerNew
+//
+//  Created by Mohosin Islam Palash on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
